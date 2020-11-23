a5adef818d312f994820e4b51726f881ef619955
Tomas Nascimento <nascimento1188@gmail.com>
2020-11-23 19:37:02 +0000

    2º commit projeto de SO

--- Parte2-SO/cliente.c
+++ Parte2-SO/cliente.c
@@ -3,10 +3,15 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "consulta.h"
 //#include <stdlib.h>
 //#include <signal.h>
 //#include <unistd.h> para a função alarm()
+
+int b;
+int veracidade=0;
 
 //Consulta nova_consulta() {
 void nova_consulta() {
@@ -20,60 +25,81 @@ void nova_consulta() {
     n.pid_consulta=getpid();
     printf ("%d\n", n.pid_consulta);
 	//return n;
-    FILE *ft=fopen("PedidoConsulta.txt", "a");
-    fprintf(ft, "%d %s %d\n", n.tipo, n.descricao, n.pid_consulta);
-    fclose(ft);
-    FILE *fp;
-    fp = fopen("SrvConsultas.pid", "r");
-    //if (fp == NULL) {
-    //    perror("Erro");
-    //    exit(1);
-    //}
-    char a[50];
-    fgets(a, sizeof(a), fp);
-    //atoi semelhante ao (int) (char) em java
-    int b = atoi(a);
-    kill (b, SIGUSR1);
-    fclose (fp);
-    //return n;
+    FILE *fb;
+    if ((fb = fopen("PedidoConsulta.txt", "r"))) {
+        fclose(fb);
+        printf("Erro! Ficheiro PedidoConsulta.txt não está vazio!\n");
+        alarm(10);
+        FILE *ft=fopen("PedidoConsulta.txt", "a");
+        fprintf(ft, "%d %s %d\n", n.tipo, n.descricao, n.pid_consulta);
+        fclose(ft);
+    } else {
+        FILE *ft=fopen("PedidoConsulta.txt", "a");
+        fprintf(ft, "%d %s %d\n", n.tipo, n.descricao, n.pid_consulta);
+        fclose(ft);
+        FILE *fp;
+        fp = fopen("SrvConsultas.pid", "r");
+        //if (fp == NULL) {
+        //    perror("Erro");
+        //    exit(1);
+        //}
+        char a[50];
+        fgets(a, sizeof(a), fp);
+        //atoi semelhante ao (int) (char) em java
+        b = atoi(a);
+        //printf ("%d\n", b);
+        fclose (fp);
+        kill (b, SIGUSR1);
+        //return n;
+    }
 }
 
 void trata_sinal (int sinal) {
     //SIGUSR2 é 12
-    if (sinal == SIGUSR2) {
+    if (sinal == 12) {
         printf ("Consulta não é possível para o processo %d\n", getpid());
         remove("PedidoConsulta.txt");
         exit (-1);
     }
     //SIGHUP é 1
-    if (sinal == SIGHUP) {
+    if (sinal == 1) {
+        veracidade=1;
         printf ("Consulta iniciada para o processo %d\n", getpid());
         remove("PedidoConsula.txt");
+        //int status;
+        //waitpid(getpid(), &status, 0);
         //pause() ??
     }
-    if (sinal == SIGTERM) {
-        int i;
-        //usar o fork
-        i=fork();
-        if (i == SIGHUP) {
-            printf ("Consulta concluída para o processo %d\n", getpid());
-        } else {
-            printf ("Não foi iniciada nenhuma consulta!");
-        }
+    //SIGTERM é 15
+    if ((sinal == 15) && (veracidade == 1)) {
+        printf ("Consulta concluída para o processo %d\n", getpid());
+        //printf ("Não foi iniciada nenhuma consulta!");
+        nova_consulta();
+    }
+    //SIGINT é 2
+    if (sinal == SIGINT) {
+        printf ("O cliente cancelou o pedido!\n");
+        remove ("PedidoConsulta.txt");
+        exit (0);
     }
 }
 
 int main () {
     //Consulta new;
     //new=nova_consulta();
+    //temporario
+    remove ("PedidoConsulta.txt");
     nova_consulta();
-    FILE *fb;
-    int pid;
-    fb = fopen("SrvConsultas.pid", "r");
-    fscanf(fb, "%d", &pid);
-    fclose (fb);
+   // FILE *fb;
+  //  int pid;
+   // fb = fopen("SrvConsultas.pid", "r");
+  // fscanf(fb, "%d", &pid);
+   // fclose (fb);
     //kill (-pid, SIGUSR1);
+    char s[100];
     signal (SIGHUP, trata_sinal);
     signal (SIGTERM, trata_sinal);
     signal (SIGUSR2, trata_sinal);
+    signal (SIGINT, trata_sinal);
+	while (1);
 }
--- Parte2-SO/servidor.c
+++ Parte2-SO/servidor.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "consulta.h"
 
 int c1=0;
@@ -10,35 +12,110 @@ int c2=0;
 int c3=0;
 //consulta perdida
 int cp=0;
+int p;
 Consulta lista_consultas[10];
 
-void receber_consulta (int signal) {
-    if (signal = SIGUSR1) {
-        Consulta n;
-        FILE *ft=fopen("PedidoConsulta.txt", "a");
-        fscanf(ft, "%d %s\n", n.tipo, n.descricao);
-        fclose(ft); 
-        printf("Chegou novo pedido de consulta do tipo %d, descricao %s e PID <pid_consulta>\n", &n.tipo, n.descricao);
-        int i=c1+c2+c3;
-        if ((c1+c2+c3) > 10) {
-            lista_consultas[i]=n;
+void inicio_lista() {
+	for (int i=0; i < 10; i++) {
+		Consulta n;
+		n.tipo=-1;
+		lista_consultas[i]=n;	
+	}		
+}
 
+void receber_consulta (int sinal) {
+    printf("Joel\n");
+    //if (signal = SIGUSR1) {
+    if (sinal = 10) {
+        Consulta n;
+        //char *linha[100];
+        //FILE *ft=fopen("PedidoConsulta.txt", "r");
+        //fscanf(ft, "%s\n", linha);
+        //fclose(ft); 
+        //n.tipo = atoi(strtok(linha, " "));
+        //printf ("%s\n", n.tipo);
+        FILE *ft=fopen("PedidoConsulta.txt", "r");
+        fscanf(ft, "%d %s %d\n", &n.tipo, n.descricao, &n.pid_consulta);
+        //fclose(ft); 
+        printf("Chegou novo pedido de consulta do tipo %d, descricao %s e PID %d\n", n.tipo, n.descricao, n.pid_consulta);
+        int i=c1+c2+c3;
+        fclose(ft); 
+        if (i < 10) {
+            lista_consultas[i]=n;
+				printf("Consulta agendada para a sala %d\n", &i);
+				if (n.tipo == 1) {
+					c1++;
+				} else if (n.tipo == 2) {
+					c2++;
+				} else if (n.tipo == 3) {
+					c3++;
+				}
+				int pid_filho=fork();
+                int status;
+                if (pid_filho == 0) {
+                    printf("mambos do filho\n");
+                    printf("Consulta iniciada na sala %d\n", i);
+                    kill (n.pid_consulta, SIGHUP);
+                    sleep(10);
+                    printf("Consulta terminada na sala %d\n", i);
+                    kill (n.pid_consulta, SIGTERM);	
+                    exit (EXIT_SUCCESS);
+                    //exit(0);
+                }
+                waitpid(pid_filho, &status, 0);
+				//sleep(10);
+                //ctrl k c
+                // printf("Consulta terminada na sala %i", &i);
+                // kill (n.pid_consulta, SIGTERM);	
         } else {
             printf("Lista de consultas cheias\n");
             //mandar o signal SIGUSR2 ao processo <pid_consulta>
+            kill (n.pid_consulta, SIGUSR2);
             cp++;
         }
     }
+    //SIGINT sinal emitido quando admin faz ctrl c
+	if (sinal == SIGINT) {
+		remove ("SrvConsultas.pid");
+		FILE *fb=fopen("StatsConsultas.dat", "a");
+		fwrite(&c1, sizeof(c1), 1, fb);
+		fwrite(&c2, sizeof(c2), 1, fb);
+		fwrite(&c3, sizeof(c3), 1, fb);
+		fwrite(&cp, sizeof(cp), 1,fb);
+		fclose(fb);
+		exit(0);
+	}
+}
 
-
+void criar_lista () {
+    Consulta s;
+    s.tipo = -1;
+    //for (int t=0; 10 < sizeof(lista_consultas)/sizeof(Consulta); t++) {
+    for (int t=0; t < 10; t++) {
+            lista_consultas[t]=s;
+            printf("joel %d\n", t);
+    }
 }
 
 int main () {
-    int p = getpid();
+    criar_lista();
+    p = getpid();
+    printf ("%p\n", p);
     FILE *fb;
-    fb = fopen("SrvConsultas.pid", "a");
-    fprintf(fb, "%d", &p);
-    fclose (fb);
+    fb = fopen("SrvConsultas.pid", "w");
+    fprintf(fb, "%d\n", p);
+    fclose(fb);
+    char s[100];
     signal (SIGUSR1, receber_consulta);
+	while (1);
+    //signal (SIGUSR1, receber_consulta);
+
+    //int p = getpid();
+    //FILE *fb;
+    //fb = fopen("SrvConsultas.pid", "a");
+    //fprintf(fb, "%d", &p);
+    //fclose (fb);
+    //signal (SIGUSR1, receber_consulta);
     signal (SIGHUP, receber_consulta);
+    signal (SIGINT, receber_consulta);
 }
